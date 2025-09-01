6bdbd6fab4547f7477feea469554a2f250d18359
Brian Rodriguez <brianrodriguez052006@gmail.com>
2025-09-01 09:17:29 -0500

    Agregar ruta de prueba en web.php

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/prueba', function () {
+    return '¡Ruta de prueba funcionando!';
+});
+
